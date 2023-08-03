bda9b4226c01afd5204010f28083d574bc9f8759
Yunhe <marvelousmyh@126.com>
2023-08-03 23:55:03 +0100

    initial import

---
+++ prompt2/filterbysize/job.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /home/s2448375/miniconda3/bin/activate pt
+python caption1.py
---
+++ prompt2/filterbysize/job2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /home/s2448375/miniconda3/bin/activate pt
+python caption35.py
